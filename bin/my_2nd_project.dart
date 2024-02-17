9a4dc9e98d668e496640bb248c0522e91e438547
hasib <hasibmohiuddin94@gmail.com>
2024-02-17 15:48:52 +0600

    added

---
+++ bin/my_2nd_project.dart
@@ -0,0 +1,249 @@
+void main (){
+
+   int x= 10;
+   int y= 50;
+   int z= x+y;
+ print(z);
+
+    String Myself = "My name is Hasib";
+
+ print(Myself);
+
+    bool areyoustudent = true;
+
+ print(areyoustudent);
+
+    List footballer=['Salah','Messi','Filx','Mane','Diaz',13,5,true ];
+
+ print(footballer);
+
+ print(footballer[4]);
+
+    Map myself={
+      'Name':'Hasib',
+      'age': 20,
+      'Height':'5 feet 7 inch',
+      'city':'Chandpur',
+      'Country':'Bangladesh'
+    };
+
+ print( myself);
+ print( myself['Country']);
+
+    int fristNumbe= 18;
+    int secondNumber= 56;
+
+ print(fristNumbe+secondNumber);
+ print(fristNumbe-secondNumber);
+ print(fristNumbe*secondNumber);
+ print(fristNumbe/secondNumber);
+ print(fristNumbe%secondNumber);
+
+    int frist_item= 10;
+
+
+    String postfix='there is postfix_1';
+
+    print(postfix);
+
+
+
+
+ print(frist_item++);
+ print(frist_item++);
+ print(frist_item++);
+ print(frist_item++);
+ print(frist_item++);
+ print(frist_item++);
+
+    String prefix='there is prefix_1';
+
+    print(prefix);
+
+
+
+
+   print(++frist_item);
+   print(++frist_item);
+   print(++frist_item);
+   print(++frist_item);
+   print(++frist_item);
+   print(++frist_item);
+
+
+
+    String unary1='there is postfix_2';
+
+print(unary1);
+
+print(frist_item--);
+print(frist_item--);
+print(frist_item--);
+print(frist_item--);
+print(frist_item--);
+print(frist_item--);
+
+
+
+   String unary2='there is prefix_2';
+
+   print(unary2);
+
+
+
+
+
+print(--frist_item);
+   print(--frist_item);
+   print(--frist_item);
+   print(--frist_item);
+   print(--frist_item);
+   print(--frist_item);
+   print(--frist_item);
+
+ final ex_date='may 2025';
+
+print('may 2025');
+
+  List<String> best_winger=['salah','vini jr','chiesa','saka','grilish','rodrigo','khavara'];
+
+   var ruselt=best_winger.runtimeType;
+
+print(ruselt);
+
+ Map<int,String> student= { 1:'hasib',2:'rakib'};
+
+print(student);
+
+ student.values;
+
+print(student.values);
+
+  List studentNo=['hasib','rakib','samir','hasan'];
+
+  studentNo.sort();
+
+print(studentNo);
+
+studentNo=['misha','rina','munni','hasina'];
+
+print(studentNo);
+
+   studentNo.add('roji');
+studentNo.addAll(['sani','kabir','shing','rki',]);
+print(studentNo);
+
+
+ Map<int,String> student_dieltes={
+   1: 'hasib',
+   5:'rakib',
+   8:'samir',
+
+ };
+
+ print(student_dieltes);
+
+
+student_dieltes.addAll(
+  { 3: 'roni',
+    4:'debu',
+    7:'ranu',
+    6:'nunez'
+  }
+);
+
+print(student_dieltes);
+
+
+  int student1cgpa= 4;
+int student2cgpa= 5;
+
+
+int rueslt= student1cgpa+student2cgpa;
+
+print(rueslt);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
