ebbdc5dab7400c159014db226ace354d02e98634
hasib <hasibmohiuddin94@gmail.com>
2024-02-24 17:34:31 +0600

    all added

---
+++ bin/if else logicall ops.dart
@@ -0,0 +1,31 @@
+void main(){
+  int mark= 34;
+  if(mark >= 90 && mark <=100){
+    print('golden A+');
+  }else if(mark >= 80 && mark <90){
+    print('A+');
+  }else if(mark >= 70 && mark <80){
+    print('A');
+  }else if(mark >= 60 && mark <70){
+    print('A-');
+  }else if(mark >= 50 && mark <60){
+    print('B');
+  }else if(mark >= 40 && mark <50){
+    print('C');
+  }else if(mark >= 33 && mark <40){
+    print('D');
+  }else if(mark >=0 && mark < 33){
+    print ('f');}else {
+    print(null);
+  }
+
+ int lottary= 33;
+  if(lottary==20|| lottary==50|| lottary==70|| lottary==56){
+    print('winer');
+  }else{
+    print('fail');
+  }
+
+
+
+}
